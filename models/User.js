d1dd1f18c834b6c6a9292e5737a47aaf236b9e8f
david-m10 <musialik.przemek@gmail.com>
2018-11-13 23:24:02 +0100

    Add User model

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    requierd: true
+  },
+
+  email: {
+    type: String,
+    requierd: true
+  },
+
+  password: {
+    type: String,
+    requierd: true
+  },
+
+  avatar: {
+    type: String,
+    requierd: true
+  },
+
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongoose.model('users', userSchema);
